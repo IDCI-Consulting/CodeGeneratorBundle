b3ac770cd6a00c6fa13e5be0d667a2f3b3f52c63
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2015-07-13 15:15:01 +0200

    Add missing file to the bundle

---
+++ IDCICodeGeneratorBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @author:  Baptiste BOUCHEREAU <baptiste.bouchereau@idci-consulting.fr>
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @license: MIT
+ */
+
+namespace IDCI\Bundle\CodeGeneratorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class IDCICodeGeneratorBundle extends Bundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+
+    }
+}
