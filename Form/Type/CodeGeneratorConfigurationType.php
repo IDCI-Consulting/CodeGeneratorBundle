07930fa8ae83c03bc7117d6965adfaac1f3b1306
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2015-07-27 15:40:33 +0200

    Add form

---
+++ Form/Type/CodeGeneratorConfigurationType.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @license: MIT
+ */
+
+namespace IDCI\Bundle\CodeGeneratorBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use IDCI\Bundle\CodeGeneratorBundle\Form\GenerationConfigurationType;
+
+class CodeGeneratorConfigurationType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('quantity', 'integer')
+            ->add('configuration', new GenerationConfigurationType())
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'idci_code_generator_configuration';
+    }
+}
